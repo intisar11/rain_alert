f615e8e14c09b5cd0a51999dbac75a9f671c7eeb
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-05-26 11:20:50 -0700

    final

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 import requests
+from twilio.rest import Client
+
 Api_keys = "a0f87bc7881e2aa18d3bd4d7bc6a465b"
+account_sid = 'ACf2ee3320ca14b2a20063a53f0e375839'
+auth_token = 'bc77a48c06d96cf74c52574a8f40dfbb'
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall?lat=34.052235&lon=-118.243683&exclude=current,minutely,daily,alerts&appid=a0f87bc7881e2aa18d3bd4d7bc6a465b")
+response.raise_for_status()
+
+data_2 = (response.json()['hourly'][0:11])
+
+weather = []
+for _ in data_2:
+    weather_id =(_['weather'])
+    for _ in weather_id:
+        chances_rain = (_['id'])
+        weather.append(chances_rain)
+
+will_rain = False
+
+for _ in weather:
+    if _ > 700:
+        will_rain = True
 
 
-print(response.json()["hourly"])
+if will_rain:
+    Client = Client(account_sid, auth_token)
+    message = Client.messages.create(body="Bring an unbrealla",from_="+17745003472",
+                                     to="+18182670211")
+    print(message.sid)
+
+
+
