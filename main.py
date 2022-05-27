3cfbb2cc22708619074492d2a558738d91a92312
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-05-26 17:49:31 -0700

    final

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
+import os
 from twilio.rest import Client
 
-Api_keys = "a0f87bc7881e2aa18d3bd4d7bc6a465b"
-account_sid = 'ACf2ee3320ca14b2a20063a53f0e375839'
-auth_token = 'bc77a48c06d96cf74c52574a8f40dfbb'
+Api_keys = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get('AUTHEN_TOKEN')
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall?lat=34.052235&lon=-118.243683&exclude=current,minutely,daily,alerts&appid=a0f87bc7881e2aa18d3bd4d7bc6a465b")
 response.raise_for_status()
